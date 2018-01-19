6a0cd2aae7351d2a2a3bd8f2d7e083daa875bc8c
allamrisha <allam.risha@gmail.com>
2018-01-18 23:40:05 -0600

    basic modules commit

---
+++ app/src/main/java/com/arm07/android/smellstastyy/ui/ListFragment.java
@@ -0,0 +1,39 @@
+package com.arm07.android.smellstastyy.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arm07.android.smellstastyy.util.ListAdapter;
+import com.arm07.android.smellstastyy.util.LoggingFragment;
+import com.arm07.android.smellstastyy.R;
+
+public class ListFragment extends Fragment {
+
+    public interface OnListRecipeSelected{
+        void onRecipeSelected(int index);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.d(LoggingFragment.TAG,"onCreateView");
+        OnListRecipeSelected listener= (OnListRecipeSelected) getActivity();
+
+        View view=inflater.inflate(R.layout.fragment_list,container,false);
+        //Recycler view is inflated into the view, adapter is attached and layout manager is passed to the view
+        RecyclerView recyclerView=(RecyclerView)view.findViewById(R.id.listRecyclerView);
+        ListAdapter adapter=new ListAdapter(listener);
+        recyclerView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/arm07/android/smellstastyy/util/ListAdapter.java
@@ -0,0 +1,63 @@
+package com.arm07.android.smellstastyy.util;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.arm07.android.smellstastyy.R;
+import com.arm07.android.smellstastyy.model.Recipes;
+import com.arm07.android.smellstastyy.ui.ListFragment;
+
+
+public class ListAdapter extends RecyclerView.Adapter{
+    private final ListFragment.OnListRecipeSelected mListener;
+
+    //private final ListFragment.OnListRecipeSelected mListener;
+
+    public ListAdapter(ListFragment.OnListRecipeSelected listener) {
+            mListener=listener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((ListViewHolder)holder).bindView(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return Recipes.names.length;
+    }
+
+    private class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView mTextView;
+        private ImageView mImageView;
+        private int mIndex;
+
+        public ListViewHolder(View itemView) {
+            super(itemView);
+            mTextView=(TextView)itemView.findViewById(R.id.itemText);
+            mImageView=(ImageView)itemView.findViewById(R.id.itemImage);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bindView(int position){
+            mIndex=position;
+            mTextView.setText(Recipes.names[position]);
+            mImageView.setImageResource(Recipes.resourceIds[position]);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onRecipeSelected(mIndex);
+        }
+    }
+}
